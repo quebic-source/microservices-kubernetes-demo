055b5d122a641e8e126516747a509a0615ffda37
tara <tara@localhost.localdomain>
2019-06-18 03:58:58 -0400

    update App

--- apps/products-service/src/main/java/com/qb/poc/products/App.java
+++ apps/products-service/src/main/java/com/qb/poc/products/App.java
@@ -2,7 +2,6 @@ package com.qb.poc.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,7 +12,6 @@ public class App {
 	}
 
 	@Bean
-	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
--- apps/reviews-service/src/main/java/com/qb/poc/reviews/App.java
+++ apps/reviews-service/src/main/java/com/qb/poc/reviews/App.java
@@ -2,11 +2,8 @@ package com.qb.poc.reviews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-// since use Eureka for now disable k8 discovery client.
-//@EnableDiscoveryClient
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
